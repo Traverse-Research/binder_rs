41045c6dfbc55d314afe9cf5bdd17e4609b67edd
Marijn Suijten <marijn@traverseresearch.nl>
2024-03-19 16:36:58 +0100

    binder_ndk_sys: Allow code to build inside a workspace
    When this file is generated into `target/`, it might still think it
    belongs to a parent `[workspace]` crate and fail to compile.
    
    This error also was not caught.

--- binder_ndk_sys/build.rs
+++ binder_ndk_sys/build.rs
@@ -17,6 +17,8 @@ rust-version = "1.67"
 
 [lib]
 crate-type = ["cdylib"]
+
+[workspace]
 "#;
 
 fn build_stub() -> Result<()> {
@@ -42,7 +44,7 @@ fn build_stub() -> Result<()> {
     f.flush()?;
 
     let target = env::var("TARGET")?;
-    Command::new("cargo")
+    let s = Command::new("cargo")
         .arg("build")
         .arg("--target")
         .arg(&target)
@@ -52,6 +54,8 @@ fn build_stub() -> Result<()> {
         .arg(&outdir)
         .current_dir(&project_path)
         .status()?;
+
+    assert!(s.success(), "{s:?}");
 
     // we always use debug build for stub due to speed!
     println!(
